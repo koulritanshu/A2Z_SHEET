a8a224693274dd1eb3220ba4baadf26641a1147c
Ritanshu <ritanshu.koul@gmail.com>
2024-06-15 20:36:23 +0530

    Djikstra without visited array

--- Graphs/Shortest_Path/23_Djikstra_Algorithm.cpp
+++ Graphs/Shortest_Path/23_Djikstra_Algorithm.cpp
@@ -1,6 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class WithoutVisitedArray
+{
+public:
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        vector<int> ans(V,INT_MAX);
+        pq.push({0,S});
+        while(!pq.empty()){
+            int fr = pq.top().second;
+            int co = pq.top().first;
+            ans[fr] = min(ans[fr],co);
+            pq.pop();
+            for(auto &i:adj[fr]){
+                if(co+i[1]<ans[i[0]]){
+                    ans[i[0]] = co+i[1];
+                    pq.push({co+i[1],i[0]});
+                }
+            }
+        }
+        return ans;
+    }
+};
+
 class Solution
 {
 public:
