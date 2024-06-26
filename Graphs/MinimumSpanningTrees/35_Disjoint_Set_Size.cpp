356975d2c69e1441989f8160e5df2c0db2c38c0a
Ritanshu <ritanshu.koul@gmail.com>
2024-06-26 17:06:57 +0530

    Disjoint Set - Union By Rank + Union By Size

---
+++ Graphs/MinimumSpanningTrees/35_Disjoint_Set_Size.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class DisjointSetSize{
+    vector<int> size, parent;
+public:
+    DisjointSetSize (int n) {
+        size.resize(n+1,1);
+        parent.resize(n+1);
+        for(int i=0;i<=n;i++){
+            parent[i] = i;
+        }
+    }
+    int findUPar(int node) {
+        if(node == parent[node]){
+            return node;
+        }
+        return parent[node] = findUPar(parent[node]);
+    }
+    void unionBySize(int u, int v){
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if(ulp_u == ulp_v) return;
+        if(size[ulp_u] > size[ulp_v]){
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+        else{
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+    }  
+    bool connectedComponents(int u, int v){
+        return (findUPar(u)==findUPar(v));
+    }
+};
+
+int main(){
+}
