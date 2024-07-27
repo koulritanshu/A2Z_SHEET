aa1a34e1fd6b84833fbcc7bcdfe7ca98325c31d6
Ritanshu <ritanshu.koul@gmail.com>
2024-07-28 02:56:11 +0530

    Largest BST

--- Trees_BST/10_LargestBST.cpp
+++ Trees_BST/10_LargestBST.cpp
@@ -1,8 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct TreeNode{
-    int val;
-    TreeNode* left;
-    TreeNode* right;
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+class NodeValue{
+public:
+    int mini,maxi,size;
+    NodeValue(int a, int b, int c){
+        mini = a;
+        maxi = b;
+        size = c;
+    }
+};
+
+class Solution{
+public:
+    NodeValue func(Node* root){
+        if(!root) return NodeValue(INT_MAX,INT_MIN,0);
+        auto left = func(root->left);
+        auto right = func(root->right);
+        if(root->data > left.maxi && root->data < right.mini){
+            return NodeValue(min(root->data,left.mini),max(root->data,right.maxi),right.size+left.size+1);
+        }
+        return NodeValue(INT_MIN,INT_MAX,max(left.size,right.size));
+    }
+    int largestBst(Node *root)
+    {
+        return func(root).size;
+    }
 };
